dcd70c6262ac014e3f1dba580c8c0ecae5db7216
Jared <jaredpoole123@gmail.com>
2021-04-29 21:40:48 -0400

    Removed debug statements

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+// import { wait } from './wait'
 
 async function run(): Promise<void> {
     try {
-        const ms: string = core.getInput('milliseconds')
-        core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
+        const in_file: string = core.getInput('coverxygen-in')
+        core.info(`Input file is ${in_file}`)
+
+        /*
         core.debug(new Date().toTimeString())
         await wait(parseInt(ms, 10))
         core.debug(new Date().toTimeString())
+        */
 
         core.setOutput('time', new Date().toTimeString())
     } catch (error) {
